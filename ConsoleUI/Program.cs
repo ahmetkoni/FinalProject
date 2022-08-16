e7c426762b7dc8a54ec2e85c047dcb1668663990
ahmetkoni <ahmetkoni7@gmail.com>
2022-08-16 15:25:23 +0300

    Proje dosyası ekle.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,8 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+foreach (var product in productManager.GetAll())
+{
+    Console.WriteLine(product.ProductName,product.UnitPrice);
+}
